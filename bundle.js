cd8cd8dfc86769be223263255b39bc0c30593365
Seth Berrier <berriers@uwstout.edu>
2023-07-10 11:14:31 -0500

    Audit for security updates - updated params.php to bootstrap 5.3

--- bundle.js
+++ bundle.js
@@ -1,8 +1,9 @@
 import ESBuild from 'esbuild'
 import { sassPlugin } from 'esbuild-sass-plugin'
 
-// Is this a development run?
+// Is this a development run & should we serve public?
 const _DEV_ = process.argv.some(env => env === 'dev')
+const _SERVER_ = process.argv.some(env => env === 'server')
 
 // Configuration for ESBuild
 const esBuildConfig = {
@@ -22,12 +23,18 @@ try {
   if (_DEV_) {
     const ctx = await ESBuild.context(esBuildConfig)
     await ctx.watch()
-    await ctx.serve({
-      port: 3000,
-      servedir: 'public'
-    })
+    if (_SERVER_) {
+      // Dynamically serve changes
+      await ctx.serve({
+        port: 3000,
+        servedir: 'public'
+      })
+    } else {
+      // Watch and rebuild, but don't serve files
+      await ESBuild.build(esBuildConfig)
+    }
   } else {
-    // In production mode, we just want to build the files
+    // In production mode, we just want to build the files once (no watch)
     await ESBuild.build(esBuildConfig)
   }
 } catch (err) {
